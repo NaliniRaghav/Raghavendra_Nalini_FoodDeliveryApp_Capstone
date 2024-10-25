9046454c535f586a36baea9bd02e2dee683516e3
Nalini Raghavendra <naliniraghavendra@nalinis-new-mbp.lan>
2024-10-24 22:21:40 -0400

    third

--- FoodDeliveryApp_frontend/src/App.jsx
+++ FoodDeliveryApp_frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import LoginPage from './pages/LoginPage';
 import RestaurantDisplayForm from './pages/RestaurantDisplayForm';
 import ResetPasswordPage from './pages/PasswordResetPage';
+import SignupPage from './pages/SignUpForm';
 
 function App() {
   const [currentForm, setCurrentForm] = useState('login');  
@@ -11,6 +12,7 @@ function App() {
       {currentForm === 'login' && <LoginPage setCurrentForm={setCurrentForm} />}
       {currentForm === 'restaurantDisplay' && <RestaurantDisplayForm />}
       {currentForm === 'reset' && <ResetPasswordPage setCurrentForm={setCurrentForm} />}
+      {currentForm === 'signup' && <SignupPage setCurrentForm={setCurrentForm} />}
     </div>
   );
 }
